19f21a7a4513273d97871a8080c807afffda762b
Bart Roos <bart@studiokaa.co>
2017-11-17 21:18:49 +0100

    Split js to seperate file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,27 +56,7 @@
 
     @if(Gate::allows('admin'))
     	<script src="https://js.pusher.com/4.1/pusher.min.js"></script>
-  		<script>
-		    var pusher = new Pusher('f80c5128c14ec84e1da9', {
-		      	cluster: 'eu',
-		      	encrypted: true
-		    });
-
-		    var channel = pusher.subscribe('whiteboard');
-		    channel.bind('signup', function(data) {
-		    	var category = document.getElementById("category-" + data.category);
-		    	var li = document.createElement('li');
-		    	li.id = "category-" + data.category + "-user-" + data.user.id;
-		    	li.innerHTML = data.user.name;
-		    	li.innerHTML += '<a class="pull-right glyphicon glyphicon-remove" href="/signoff/user/' + data.user.id + '/category/' + data.category + '"></a>';
-		    	console.log(li);
-		    	category.appendChild(li);
-		    });
-		    channel.bind('signoff', function(data) {
-		    	var li = document.getElementById("category-" + data.category + "-user-" + data.user.id);
-		    	li.parentNode.removeChild(li);
-		    });
-	  	</script>
+  		<script src="{{ asset('js/websockets.js') }}"></script>
     @endif
 </body>
 </html>
